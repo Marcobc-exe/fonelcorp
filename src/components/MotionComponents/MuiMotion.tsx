bc7934462dea784780121d2307b41a565bae1e88
Marcobc-exe <marco.developing@gmail.com>
2025-05-13 17:33:26 -0600

    fix: Fixing warnings from typescript

--- src/components/MotionComponents/MuiMotion.tsx
+++ src/components/MotionComponents/MuiMotion.tsx
@@ -6,3 +6,4 @@ export const MotionBox = motion.create(Box);
 export const MotionTypography = motion.create(Typography);
 export const MotionCard = motion.create(Card);
 export const MotionGrid = motion.create(Grid);
+export const MotionImg = motion.create('img');
