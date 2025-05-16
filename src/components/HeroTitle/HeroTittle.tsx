7b9aa0a88fc699bb75fe16f5873dbba193fbe1f6
Marcobc-exe <marco.developing@gmail.com>
2025-05-16 01:03:03 -0600

    style: Handle HeroTitle size

--- src/components/HeroTitle/HeroTittle.tsx
+++ src/components/HeroTitle/HeroTittle.tsx
@@ -1,16 +1,8 @@
-import { Box, useMediaQuery, useTheme } from "@mui/material";
 import "./hero.css";
-import { Typography } from "../Typography/Typography";
+import { Box } from "@mui/material";
 import { MotBox } from "../MotionComponents/CustomsMuiMotion";
 
 export const HeroTitle = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
-  const isDesktop = useMediaQuery(theme.breakpoints.between("md", "lg"));
-  const isDesktopXl = useMediaQuery(theme.breakpoints.between("lg", "xl"));
-  const isDesktopXxl = useMediaQuery(theme.breakpoints.up("xl"));
-
   const handleCallNow = () => {
     const el = document.getElementById("contact");
     if (el) el.scrollIntoView({ behavior: "smooth", block: "start" });
@@ -23,10 +15,7 @@ export const HeroTitle = () => {
       component="div"
       height={{
         xs: "400px",
-        sm: "500px",
-        md: "600px",
-        lg: "700px",
-        xl: "800px",
+        xl: "400px",
       }}
     >
       <Box
@@ -45,169 +34,29 @@ export const HeroTitle = () => {
         src={"./images/title-img-cloud.png"}
         alt="Cloud"
       />
-      {isMobile && (
-        <MotBox
-          id="isMobile"
-          className="box-hero-title"
-          component={"div"}
-          height={{
-            xs: "300px",
-            sm: "320px",
-          }}
-          width={{
-            xs: "200px",
-          }}
-          sx={{
-            marginLeft: "12px",
-            marginTop: "12px",
-          }}
-        >
-          <Typography
-            variant="h6"
-            sx={{
-              fontSize: "2rem",
-              fontWeight: "bold",
-              lineHeight: 1.2,
-              marginBottom: "4px",
-            }}
-          >
-            Roadside Assistance
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{
-              fontSize: "1rem",
-              fontWeight: "bold",
-              marginBottom: "14px",
-            }}
-          >
-            24/7 Reliable
-          </Typography>
-          <Typography variant="h5">Calgary &</Typography>
-          <Typography variant="h5">Edmonton</Typography>
-          <button className="btn-contact-now" onClick={() => handleCallNow()}>
-            Call now!
-          </button>
-        </MotBox>
-      )}
-      {isTablet && (
-        <MotBox
-          id="isTablet"
-          className="box-hero-title"
-          component={"div"}
-          height={{
-            sm: "320px",
-            md: "350px",
-          }}
-          width={{
-            sm: "400px",
-            md: "500px",
-          }}
-          sx={{
-            marginLeft: "12px",
-            marginTop: "12px",
-          }}
-        >
-          <Typography variant="h2">
-            24/7 Reliable Roadside Assistance
-          </Typography>
-          <Typography variant="subtitle1">Calgary & Edmonton</Typography>
-          <button
-            className="btn-contact-now subtitle1"
-            onClick={() => handleCallNow()}
-          >
-            Call now!
-          </button>
-        </MotBox>
-      )}
-      {isDesktop && (
-        <MotBox
-          id="isDesktop"
-          className="box-hero-title"
-          component={"div"}
-          height={{
-            md: "350px",
-            lg: "380px",
-          }}
-          width={{ md: "400px" }}
-          sx={{ marginLeft: "12px", marginTop: "12px" }}
-        >
-          <Typography variant="subtitle2" lineHeight={1.2}>
-            24/7 Reliable Roadside Assistance
-          </Typography>
-          <Typography variant="subtitle1">Calgary & Edmonton</Typography>
-          <button
-            className="btn-contact-now subtitle2"
-            onClick={() => handleCallNow()}
-          >
-            Call now!
-          </button>
-        </MotBox>
-      )}
-      {isDesktopXl && (
-        <MotBox
-          id="isDesktopXl"
-          className="box-hero-title"
-          component={"div"}
-          height={{
-            lg: "380px",
-          }}
-          width={{ lg: "500px" }}
-          sx={{ marginLeft: "12px", marginTop: "24px" }}
-        >
-          <Typography variant="h1" lineHeight={1.2}>
-            24/7 Reliable Roadside Assistance
-          </Typography>
-          <Typography variant="h3">Calgary & Edmonton</Typography>
-          <button
-            className="btn-contact-now subtitle3"
-            onClick={() => handleCallNow()}
-          >
-            Call now!
-          </button>
-        </MotBox>
-      )}
-      {isDesktopXxl && (
-        <MotBox
-          id="isDesktopXxl"
-          className="box-hero-title"
-          height={{
-            lg: "380px",
-          }}
-          width={{ xl: "800px" }}
-          sx={{
-            marginLeft: "24px",
-            marginTop: "54px",
-          }}
-        >
-          <Typography
-            lineHeight={1.2}
-            sx={{
-              fontWeight: 600,
-              fontSize: "5rem",
-              color: "#1F355E",
-            }}
-          >
-            24/7 Reliable Roadside Assistance
-          </Typography>
-          <Typography
-            sx={{
-              fontWeight: 600,
-              fontSize: "3rem",
-              color: "#1F355E",
-              width: "900px",
-            }}
-          >
-            Calgary & Edmonton
-          </Typography>
-          <button
-            className="btn-contact-now subtitle4"
-            onClick={() => handleCallNow()}
-          >
-            Call now!
-          </button>
-        </MotBox>
-      )}
+      <MotBox
+        id="isDesktopXxl"
+        className="box-hero-title"
+        height={{
+          xs: "300px",
+          sm: "320px",
+          md: "350px",
+          lg: "380px",
+        }}
+        width={"800px"}
+        sx={{
+          marginLeft: "24px",
+          marginTop: "44px",
+        }}
+      >
+        <h1 className="title-roadside-assistance">
+          24/7 Reliable {<br />} Roadside Assistance
+        </h1>
+        <h2 className="subtitle-calgary-edmonton">Calgary & Edmonton</h2>
+        <button className="btn-contact-now" onClick={() => handleCallNow()}>
+          Call now!
+        </button>
+      </MotBox>
     </Box>
   );
 };
